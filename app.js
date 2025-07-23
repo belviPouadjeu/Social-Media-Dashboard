98b1812033fe21c986f870ba2f6a4138fb3879db
belviPouadjeu <pbelvinard21@gmail.com>
2025-07-23 11:37:29 +0000

    feat: routing HTML added

--- app.js
+++ app.js
@@ -64,6 +64,12 @@ function requireAuth(req, res, next) {
     }
 }
 
+// Insert your routing HTML code here.
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'public', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
+app.get('/post', requireAuth, (req, res) => res.sendFile(path.join(__dirname, 'public', 'post.html')));
+app.get('/index', requireAuth, (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html'), { username: req.user.username }));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
